b4e51cb9b975b4040cc8290b1e7252c9ea7f11df
Matias Alvarez <matiasealvarez89@gmail.com>
2022-04-07 09:44:39 -0300

    Empiezo a diseñar menu

--- TP_1/src/TP_1.c
+++ TP_1/src/TP_1.c
@@ -11,72 +11,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float descuentoDebito(float *precio);
-float recargoTarjetaCredito(float precio);
-float bitcoin(float precio);
-float precioPorKilometro(float km, float precio);
+int getInt(int* pNumeroIngresado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos);
+
 
 int main(void) {
 	setbuf(stdout, NULL);
 
-	int kilometros;
-	float precioAerolineas;
-	float precioLatam;
+	int opcionMenu;
+	int kilometros = 0;
+	float precioAerolineas = 0;
+	float precioLatam = 0;
+	int validacionRetorno;
 
-	printf("Ingrese la cantidad de KM: ");
-	scanf("%d", &kilometros);
-
-	printf("Ingrese el precio por Aerolineas: $ ");
-	scanf("%f", &precioAerolineas);
-
-	printf("Ingrese el precio por Latam: $ ");
-	scanf("%f", &precioLatam);
-
-	printf("KMs Ingresados: %d\n\n", kilometros);
-
-	printf("Precio Aerolineas: $%.2f\n", precioAerolineas);
-	printf("a) Precio con tarjeta de debito: $%.2f\n", descuentoDebito(&precioAerolineas));
-	printf("b) Precio con tarjeta de credito: $%.2f\n", recargoTarjetaCredito(precioAerolineas));
-	printf("c) Precio pagando con bitcoin: %.2f BTC\n", bitcoin(precioAerolineas));
-	printf("d) Mostrar precio unitario: $%.2f\n\n", precioPorKilometro(kilometros, precioAerolineas));
-
-//	printf("Precio Latam: $%.2f\n", precioAerolineas);
-//	printf("a) Precio con tarjeta de debito: $%.2f\n", descuentoDebito(precioLatam));
-//	printf("b) Precio con tarjeta de credito: $%.2f\n", recargoTarjetaCredito(precioLatam));
-//	printf("c) Precio pagando con bitcoin: %.2f BTC\n", bitcoin(precioLatam));
-//	printf("d) Mostrar precio unitario: $%.2f\n", precioPorKilometro(kilometros, precioLatam));
+	printf("1) Ingresar Kilometros: (KM = %d)\n",kilometros);
+	printf("2) Ingresar precio de vuelos: (Aerolineas = %.2f, Latam = %.2f)\n",precioAerolineas,precioLatam);
 
 
 
-	return EXIT_SUCCESS;
-}
 
-float descuentoDebito(float* precio){
-	*precio = *precio*0.9;
 
 	return 0;
 }
 
-float recargoTarjetaCredito(float precio){
-	float resultado;
+int getInt(int * pNumeroIngresado,char * mensaje,char * mensajeError,int minimo, int maximo, int reintentos){
+	int auxNumeroIngresado;
+	int retorno;
+	retorno = -1;
 
-	resultado = precio*1.25;
+	if(pNumeroIngresado != NULL && maximo >= minimo && reintentos>= 0){
+		do{
+			printf("%s", mensaje);
+			scanf("%d", &auxNumeroIngresado);
+			reintentos--;
 
-	return resultado;
-}
+			if(auxNumeroIngresado >= minimo && auxNumeroIngresado <= maximo){
+						*pNumeroIngresado = auxNumeroIngresado;
+						retorno = 0;
+						break;
+			}else{
+				printf("%s", mensajeError);
+			}
 
-float bitcoin(float precio){
-	float resultado;
+		}while(reintentos > 0);
+	}
 
-	resultado = precio/4606954.55;
-
-	return resultado;
-}
-
-float precioPorKilometro(float km, float precio){
-	float resultado;
-
-	resultado = precio/km;
-
-	return resultado;
+	return retorno;
 }

8c8aef7668fe7501e660ed65139471588f512cdf
Matias Alvarez <matiasealvarez89@gmail.com>
2022-04-07 14:24:45 -0300

    Punto 1 y 2, menu, submenu y funciones

--- TP_1/src/TP_1.c
+++ TP_1/src/TP_1.c
@@ -12,7 +12,9 @@
 #include <stdlib.h>
 
 int getInt(int* pNumeroIngresado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos);
-
+int getFloat(float* pNumeroIngresado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos);
+int menu(int distancia, float precio1, float precio2);
+int sumbenuPrecio(float* precio1,float* precio2);
 
 int main(void) {
 	setbuf(stdout, NULL);
@@ -23,29 +25,52 @@ int main(void) {
 	float precioLatam = 0;
 	int validacionRetorno;
 
-	printf("1) Ingresar Kilometros: (KM = %d)\n",kilometros);
-	printf("2) Ingresar precio de vuelos: (Aerolineas = %.2f, Latam = %.2f)\n",precioAerolineas,precioLatam);
+	do{
+		menu(kilometros,precioAerolineas,precioLatam);
+
+		validacionRetorno = getInt(&opcionMenu,"Ingrese la opcion deseada: \n","Ingreso incorrecto\n",1,6,5);
+
+		if(validacionRetorno == 0){
+			switch(opcionMenu){
+				case 1:
+					validacionRetorno = getInt(&kilometros,"Ingrese la cantidad de kilometros: \n","Ingreso incorrecto\n",1,100000,5);
+					break;
+				case 2:
+//					validacionRetorno = getFloat(&precioAerolineas,"Ingrese el precio por Aerolineas: \n","Ingreso incorrecto\n",1,10000000,5);
+//					validacionRetorno = getFloat(&precioLatam,"Ingrese el precio por Latam: \n","Ingreso incorrecto\n",1,10000000,5);
+					sumbenuPrecio(&precioAerolineas,&precioLatam);
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+			}
 
 
-
+		}
+	}while(opcionMenu != 6);
 
 
 	return 0;
 }
 
-int getInt(int * pNumeroIngresado,char * mensaje,char * mensajeError,int minimo, int maximo, int reintentos){
-	int auxNumeroIngresado;
+int getInt(int* pNumeroIngresado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos){
+	int buffer;
 	int retorno;
 	retorno = -1;
 
 	if(pNumeroIngresado != NULL && maximo >= minimo && reintentos>= 0){
 		do{
 			printf("%s", mensaje);
-			scanf("%d", &auxNumeroIngresado);
+			scanf("%d", &buffer);
 			reintentos--;
 
-			if(auxNumeroIngresado >= minimo && auxNumeroIngresado <= maximo){
-						*pNumeroIngresado = auxNumeroIngresado;
+			if(buffer >= minimo && buffer <= maximo){
+						*pNumeroIngresado = buffer;
 						retorno = 0;
 						break;
 			}else{
@@ -56,4 +81,76 @@ int getInt(int * pNumeroIngresado,char * mensaje,char * mensajeError,int minimo,
 	}
 
 	return retorno;
+}
+
+int getFloat(float* pNumeroIngresado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos){
+	float buffer;
+	int retorno;
+	retorno = -1;
+
+	if(pNumeroIngresado != NULL && maximo >= minimo && reintentos>= 0){
+		do{
+			printf("%s", mensaje);
+			scanf("%f", &buffer);
+			reintentos--;
+
+			if(buffer >= minimo && buffer <= maximo){
+						*pNumeroIngresado = buffer;
+						retorno = 0;
+						break;
+			}else{
+				printf("%s", mensajeError);
+			}
+
+		}while(reintentos > 0);
+	}
+
+	return retorno;
+}
+
+int menu(int distancia, float precio1, float precio2){
+
+	printf("1) Ingresar Kilometros: (KM = %d)\n",distancia);
+	printf("2) Ingresar precio de vuelos: (Aerolineas = $%.2f, Latam = $%.2f)\n",precio1,precio2);
+	printf("3) Calcular todos los costos\n");
+	printf("4) Informar Resultados\n");
+	printf("5) Carga forzada de datos\n");
+	printf("6) Salir\n");
+
+	return 0;
+}
+
+int subMenuKilometros(int distancia){
+	printf("Ingrese la cantidad de km: \n");
+	scanf("%d", &distancia);
+
+	return 0;
+}
+
+int sumbenuPrecio(float* precio1,float* precio2){
+	int opcionSubMenu;
+	int validacionRetornoSubMenu;
+
+	do{
+		printf("1) Ingrese el precio por Aerolineas: \n");
+		printf("2) Ingrese el precio por Latam: \n");
+		printf("3) Salir");
+		validacionRetornoSubMenu = getInt(&opcionSubMenu,"Ingrese la opcion deseada: \n","Ingreso incorrecto\n",1,3,4);
+		if(validacionRetornoSubMenu == 0){
+			switch(opcionSubMenu){
+				case 1:
+					validacionRetornoSubMenu = getFloat(precio1,"Ingrese el precio por Aerolineas: \n","Ingreso incorrecto\n",1,10000000,5);
+					break;
+				case 2:
+					validacionRetornoSubMenu = getFloat(precio2,"Ingrese el precio por Latam: \n","Ingreso incorrecto\n",1,10000000,5);
+					break;
+				case 3:
+					break;
+			}
+		}
+
+	}while(opcionSubMenu != 3);
+
+
+	return 0;
 }
